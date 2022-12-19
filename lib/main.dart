b6af629fb67bb3dd3df670a683ff215626b48ce0
tmo321 <bhopag24@gmail.com>
2022-12-19 21:40:50 +0700

    apalah

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:resep_makanan/screen/resep_makanan_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Resep Makanan',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const ResepMakananScreen(),
+    );
+  }
+}
